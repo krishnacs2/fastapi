2d42c2067d7138871a51936d0d0ef11d1ec9d70c
krishnacs2 <krishnaiah.yelika@gmail.com>
2023-10-31 06:52:48 +0000

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,1 +1,7 @@
-gunicorn --bind=0.0.0.0 --workers=4 startup:app
+#!/bin/bash
+
+# Get the number of workers from the environment variable, default to 4 if not set
+WORKER_COUNT=${GUNICORN_CMD_ARGS:11:1} # Extracts the worker count from the environment variable
+
+# Start Gunicorn with the dynamic worker count
+gunicorn app:app --workers=$WORKER_COUNT -k uvicorn.workers.UvicornWorker
