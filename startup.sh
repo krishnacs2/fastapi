e38db28b9fac4144a59c0b3e2018ad3ee7de03ab
krishnacs2 <krishnaiah.yelika@gmail.com>
2023-10-31 07:00:08 +0000

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Get the number of workers from the environment variable
-WORKER_COUNT=${GUNICORN_CMD_ARGS:11:1} # Extracts the worker count from the environment variable
+# Calculate worker count dynamically
+WORKER_COUNT=$(python calculate_workers.py)
 
-# Start Gunicorn with the dynamic worker count
-gunicorn app:app --workers=$WORKER_COUNT -k uvicorn.workers.UvicornWorker
+# Start Gunicorn with the calculated worker count
+gunicorn app:app --workers=${WORKER_COUNT} -k uvicorn.workers.UvicornWorker
