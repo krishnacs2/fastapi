e5c0fb2305cd42ae4e1489ec0b62b024189f142d
krishnacs2 <krishnaiah.yelika@gmail.com>
2023-10-31 06:53:16 +0000

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Get the number of workers from the environment variable, default to 4 if not set
+# Get the number of workers from the environment variable
 WORKER_COUNT=${GUNICORN_CMD_ARGS:11:1} # Extracts the worker count from the environment variable
 
 # Start Gunicorn with the dynamic worker count
